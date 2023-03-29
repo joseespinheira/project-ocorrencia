3d0a5eaf2089ffd5909d57debd6ac609015de8f8
José Espinheira <joseespinheira@gmail.com>
2023-03-29 15:00:36 -0300

    Mensagem de erro

--- src/App.js
+++ src/App.js
@@ -3,13 +3,13 @@ import { Link, useNavigate } from "react-router-dom";
 import 'mapbox-gl/dist/mapbox-gl.css';
 import api from "./services";
 import { GuardarDado, RemoverItem } from "./components/Storage";
-import moment from "moment";
 import 'moment/locale/pt-br';
+import { Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle } from "@mui/material";
+import { useState } from "react";
 
 function App() {
-
-  const data = moment;
-  // data.locale('pt-br');
+  const [aviso, setAviso] = useState(false);
+  const [mensageErro, setMensageErro] = useState("");
 
   let navigate = useNavigate();
 
@@ -45,17 +45,38 @@ function App() {
     } catch (e) {
       console.log(e);
       console.log(e.message);
+      setAviso(true)
+      setMensageErro(e.message);
       console.log(e.response.data.message);
     }
   }
   return (
     <Container className="h-100">
+      <Dialog
+        open={aviso}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+    >
+        <DialogTitle id="alert-dialog-title">
+            Erro
+        </DialogTitle>
+        <DialogContent>
+            <DialogContentText id="alert-dialog-description">
+                {mensageErro}
+            </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+            <Button onClick={() => { setAviso(false); }} autoFocus>
+                Ok
+            </Button>
+        </DialogActions>
+    </Dialog>
       <Row className="h-100 justify-content-md-center align-items-center">
         <Col className="">
           <Form className="border border-primary rounded p-4" onSubmit={handleSubmit}>
             <div className="d-flex justify-content-center">
               <div className="text-center">
-                <h3>SOAPP - {data().format('LLLL')       }</h3>
+                <h3>SOAPP</h3>
                 <h6>Aplicativo de ocorrências</h6>
               </div>
             </div>
